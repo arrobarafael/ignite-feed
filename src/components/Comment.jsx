796c9d125355de0e5f3584d4f1d2e5af9289ddb0
Rafael Pelegrini <arrobarafael@hotmail.com>
2024-05-19 15:41:11 -0300

    Criada seção de comentários

---
+++ src/components/Comment.jsx
@@ -0,0 +1,36 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/arrobarafael.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Rafael Pelegrini</strong>
+              <time title="15 de maio as 08:03h" dateTime="2024-05-15 08:03:01">
+                Cerca de 1 hora atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!!</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>15</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
