2cb214007b3263259c8bba2ecacce45849c48730
Rafael Pelegrini <arrobarafael@hotmail.com>
2024-05-21 18:52:59 -0300

    Adicionado contador de aplausos

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,11 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
   }
@@ -30,9 +33,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={() => setLikeCount(likeCount + 1)}>
             <ThumbsUp />
-            Aplaudir <span>15</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
